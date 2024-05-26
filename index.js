a3e83ddade5e4cd1737f048447ed503ccd7833f8
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2024-05-26 14:32:39 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'images')));
+
+//redirect path to hash
+// app.use((req, res, next) => {
+//   if (!req.originalUrl.includes("#")) {
+//     req.redirect = `/#${req.originalUrl.slice(1)}`;
+//   }
+//   next();
+// });
+
+//for any path give index.html (doesn't work?)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/level/1', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/level1.html'));
+});
+app.get('/level/2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/level2.html'));
+});
+app.get('/level/3', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/level3.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
