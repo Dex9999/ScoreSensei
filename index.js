1fad1e3e3e8afbba7cb3ef8af45c458448359741
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2024-05-26 15:34:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname + '/public')));
-app.use(express.static(path.join(__dirname + '/images')));
-console.log(path.join(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'images')));
+
 app.use((req, res, next) => {
   console.log(`${req.method} request for '${req.url}'`);
   next();
 });
 
-//redirect path to hash
-// app.use((req, res, next) => {
-//   if (!req.originalUrl.includes("#")) {
-//     req.redirect = `/#${req.originalUrl.slice(1)}`;
-//   }
-//   next();
-// });
-
-async function checkPageForLink(req, res) {
+function checkPageForLink(req, res, next) {
   const url = req.url;
-  if (url === '/level/1' ||url === '/level/2' ||url === '/level/3'){
-    const html = fs.readFileSync('/public/level.html', 'utf8');
-    res.send(html);
-  } else{
-    const html = fs.readFileSync('/public/index.html', 'utf8');
-    res.send(html);
+  if (url === '/level/1' || url === '/level/2' || url === '/level/3') {
+    res.sendFile(path.join(__dirname, 'public', 'level.html'));
+  } else {
+    next(); // Pass control to the next middleware
   }
 }
-//for any path give index.html (doesn't work?)
-app.get('*', checkPageForLink);
-  
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+
+// Catch-all route for serving index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
