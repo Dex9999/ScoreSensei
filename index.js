07c73291d93418e8f6cdc84e39bf486336326cee
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2024-05-26 14:51:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/images')));
 
 //redirect path to hash
 // app.use((req, res, next) => {
