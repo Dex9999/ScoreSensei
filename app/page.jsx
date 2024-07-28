e814f39f947a4e92e0192204cbdb415433458c08
Declan <95934184+Dex9999@users.noreply.github.com>
2024-07-27 23:19:34 -0400

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -34,9 +34,9 @@ export default function Home() {
 
       document.querySelectorAll("img").forEach((element) => {
         if (element.id < level && element.id != "0") {
-          element.src = "/images/completebutton.png";
+          element.src = "/public/completebutton.png";
         } else if (element.id > level && element.id != "0") {
-          element.src = "/images/redbutton.png";
+          element.src = "/public/redbutton.png";
         }
       });
     }
@@ -375,19 +375,19 @@ export default function Home() {
           <div id="app"></div>
           <h1>Level One</h1>
           <a id="1" href="#level1" onClick={() => startLevel(1)}>
-            <img className="button" src="/images/playbutton.png" width="150" height="150" id="1" />
+            <img className="button" src="/public/playbutton.png" width="150" height="150" id="1" />
           </a>
           <h1 id="1">The Basics</h1>
           <br />
           <h1>Level Two</h1>
           <a id="2" href="#level2" onClick={() => startLevel(2)}>
-            <img className="button" src="/images/playbutton.png" width="150" height="150" id="2" />
+            <img className="button" src="/public/playbutton.png" width="150" height="150" id="2" />
           </a>
           <h1 id="2">New Notes</h1>
           <br />
           <h1>Level Three</h1>
           <a id="3" href="#level3" onClick={() => startLevel(3)}>
-            <img className="button" src="/images/playbutton.png" width="150" height="150" id="3" />
+            <img className="button" src="/public/playbutton.png" width="150" height="150" id="3" />
           </a>
           <h1 id="3">Sharp Notes</h1>
         </div>
@@ -395,7 +395,7 @@ export default function Home() {
         <div id="game" style={{ display: "none" }}>
           <div className="home" style={{ marginTop: "100px" }}>
             <a href="/">
-              <img src="https://scoresensei.vercel.app/images/scoresensei.png" alt="Logo" style={{ width: "70px", height: "70px" }} className="center" />
+              <img src="https://scoresensei.vercel.app/public/scoresensei.png" alt="Logo" style={{ width: "70px", height: "70px" }} className="center" />
             </a>
           </div>
           <p><button onClick={() => startPitchDetect()} className="modern-button">Start</button></p>
