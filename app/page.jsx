c050b793246ed5631f72df1c92984af66daeedfb
Declan <95934184+Dex9999@users.noreply.github.com>
2024-07-27 23:26:30 -0400

    fix images

--- app/page.jsx
+++ app/page.jsx
@@ -34,9 +34,9 @@ export default function Home() {
 
       document.querySelectorAll("img").forEach((element) => {
         if (element.id < level && element.id != "0") {
-          element.src = "/public/completebutton.png";
+          element.src = "/completebutton.png";
         } else if (element.id > level && element.id != "0") {
-          element.src = "/public/redbutton.png";
+          element.src = "/redbutton.png";
         }
       });
     }
@@ -375,19 +375,40 @@ export default function Home() {
           <div id="app"></div>
           <h1>Level One</h1>
           <a id="1" href="#level1" onClick={() => startLevel(1)}>
-            <img className="button" src="/public/playbutton.png" width="150" height="150" id="1" />
+            <img
+              className="button"
+              src="/images/playbutton.png"
+              width="150"
+              height="150"
+              id="1"
+              alt="Play Button"
+            />
           </a>
           <h1 id="1">The Basics</h1>
           <br />
           <h1>Level Two</h1>
           <a id="2" href="#level2" onClick={() => startLevel(2)}>
-            <img className="button" src="/public/playbutton.png" width="150" height="150" id="2" />
+            <img
+              className="button"
+              src="/images/playbutton.png"
+              width="150"
+              height="150"
+              id="1"
+              alt="Play Button"
+            />
           </a>
           <h1 id="2">New Notes</h1>
           <br />
           <h1>Level Three</h1>
           <a id="3" href="#level3" onClick={() => startLevel(3)}>
-            <img className="button" src="/public/playbutton.png" width="150" height="150" id="3" />
+            <img
+              className="button"
+              src="/images/playbutton.png"
+              width="150"
+              height="150"
+              id="1"
+              alt="Play Button"
+            />
           </a>
           <h1 id="3">Sharp Notes</h1>
         </div>
