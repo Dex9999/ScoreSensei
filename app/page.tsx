326337e4a571c0d2b53055306cbcd5de0f904aef
Declan <95934184+Dex9999@users.noreply.github.com>
2024-07-27 22:15:29 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div id="home">
 {/*         <h1 class="ttl" style="margin-top: 100px">Welcome to Score Sensei!</h1> */}
-        <div id="app"></div>
+        <>
+          <div id="app"></div>
         <h1>Level One</h1>
         <h1 id="1">The Basics</h1>
         <br>
@@ -14,6 +15,7 @@ export default function Home() {
         <br>
         <h1>Level Three</h1>
         <h1 id="3">Sharp Notes</h1>
+        </>
       </div>
 
     </main>
