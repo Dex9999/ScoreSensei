3dca34e69ccb908a43d3200c0574a74c5f8a8272
Declan <95934184+Dex9999@users.noreply.github.com>
2024-07-27 22:03:54 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,149 +1,23 @@
 import Image from "next/image";
-import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    async function load() {
-      let level = getCookie("level");
-      console.log(level);
-      if (!level) {
-        console.log("set");
-        document.cookie = "level=1;";
-        level = 1;
-      }
-      document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
-
-      document.querySelectorAll("h1").forEach((element) => {
-        if (Number(element.id) < level && element.id != "0") {
-          element.innerText += " Completed";
-          element.style.color = "#2df412";
-        }
-      });
-
-      document.querySelectorAll("a").forEach((element) => {
-        if (Number(element.id) > level && element.id != "0") {
-          element.href = "#";
-          element.onclick = null;
-        }
-      });
-
-      document.querySelectorAll("img").forEach((element) => {
-        if (Number(element.id) < level && element.id != "0") {
-          element.src = "/images/completebutton.png";
-        } else if (Number(element.id) > level && element.id != "0") {
-          element.src = "/images/redbutton.png";
-        }
-      });
-
-      function getCookie(cname: string) {
-        let name = cname + "=";
-        let decodedCookie = decodeURIComponent(document.cookie);
-        let ca = decodedCookie.split(";");
-        for (let i = 0; i < ca.length; i++) {
-          let c = ca[i];
-          while (c.charAt(0) == " ") {
-            c = c.substring(1);
-          }
-          if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-          }
-        }
-        return "";
-      }
-    }
-    load();
-  }, []);
-
-  async function startLevel(lvl: number) {
-    document.getElementById("home")!.style.display = "none";
-    document.getElementById("game")!.style.display = "block";
-
-    const imgSrc =
-      "https://scoresensei.vercel.app/public/" + (lvl === 1 ? "1-1" : lvl) + ".png";
-    (document.getElementById("image") as HTMLImageElement).src = imgSrc;
-    go(lvl);
-  }
-
-  async function home() {
-    document.getElementById("game")!.style.display = "none";
-    document.getElementById("home")!.style.display = "block";
-    load();
-  }
-
   return (
-    <div id="home">
-      <h1 className="ttl" style={{ marginTop: "100px" }}>Welcome to Score Sensei!</h1>
-      <div id="app"></div>
-      <h1>Level One</h1>
-      <a id="1" href="#level1" onClick={() => startLevel(1)}>
-        <Image
-          className="button"
-          src="/images/playbutton.png"
-          width={150}
-          height={150}
-          alt="Play Level 1"
-          id="1"
-        />
-      </a>
-      <h1 id="1">The Basics</h1>
-      <br />
-      <h1>Level Two</h1>
-      <a id="2" href="#level2" onClick={() => startLevel(2)}>
-        <Image
-          className="button"
-          src="/images/playbutton.png"
-          width={150}
-          height={150}
-          alt="Play Level 2"
-          id="2"
-        />
-      </a>
-      <h1 id="2">New Notes</h1>
-      <br />
-      <h1>Level Three</h1>
-      <a id="3" href="#level3" onClick={() => startLevel(3)}>
-        <Image
-          className="button"
-          src="/images/playbutton.png"
-          width={150}
-          height={150}
-          alt="Play Level 3"
-          id="3"
-        />
-      </a>
-      <h1 id="3">Sharp Notes</h1>
-    </div>
-    <div id="game" style={{ display: "none" }}>
-      <div className="home" style={{ marginTop: "100px" }}>
-        <a href="/">
-          <Image
-            src="https://scoresensei.vercel.app/images/scoresensei.png"
-            alt="Logo"
-            width={70}
-            height={70}
-            className="center"
-          />
-        </a>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        
       </div>
-      <p>
-        <button onClick={startPitchDetect} className="modern-button">Start</button>
-      </p>
-      <div id="detector" className="vague">
-        <div className="pitch" style={{ display: "none" }}>
-          <span id="pitch"></span>
-        </div>
-        <p>Your Note:</p>
-        <div className="note">
-          <span id="note"></span>
-        </div>
-        <canvas id="output" width="300" height="42"></canvas>
-        <div id="detune" style={{ display: "none" }}>
-          <span id="detune_amt">--</span>
-          <span id="flat">cents &#9837;</span>
-          <span id="sharp">cents &#9839;</span>
-        </div>
+
+      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
       </div>
-      <Image src="" alt="Game Image" id="image" width={300} height={300} />
-    </div>
+
+    </main>
   );
 }
