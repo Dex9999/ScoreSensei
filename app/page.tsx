44f75a5552b9a229cdfb9a4a0ea0f490ab1519e8
Declan <95934184+Dex9999@users.noreply.github.com>
2024-07-27 22:07:05 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,28 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div id="home">
+<!--     <img class="wlcm" src="/images/scoresensei.png" width="50" height="50" /> -->
+    <h1 class="ttl" style="margin-top: 100px">Welcome to Score Sensei!</h1>
+    <div id="app"></div>
+    <h1>Level One</h1>
+    <a id="1" href="#level1" onclick="startLevel(1)">
+      <img class="button" src="/images/playbutton.png" width="150" height="150" id="1" />
+    </a>
+    <h1 id="1">The Basics</h1>
+    <br>
+    <h1>Level Two</h1>
+    <a id="2" href="#level2" onclick="startLevel(2)">
+      <img class="button" src="/images/playbutton.png" width="150" height="150" id="2" />
+    </a>
+    <h1 id="2">New Notes</h1>
+    <br>
+    <h1>Level Three</h1>
+    <a id="3" href="#level3" onclick="startLevel(3)">
+      <img class="button" src="/images/playbutton.png" width="150" height="150" id="3" />
+    </a>
+    <h1 id="3">Sharp Notes</h1>
+  </div>
 
     </main>
   );
